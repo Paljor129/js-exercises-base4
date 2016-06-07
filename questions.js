d87aac3e278b9c99002fcfc785bbcc1ed7d7a8a9
maryse-caron60400 <maryse.caron3@wanadoo.fr>
2016-06-07 12:17:51 +0200

    JS4

--- questions.js
+++ questions.js
@@ -1,63 +1,64 @@
 /**
  * Exercice sur les chaines de caractères.
  * Trouvez la façon de faire la plus optimal.
- * Il peut y avoir plusieur façon de faire. 
+ * Il peut y avoir plusieur façon de faire.
  */
 var tailleString = function (texte) {
-    return 'A completer';
+    return texte.length;
 }
 var remplaceECar = function (texte) {
-    return 'A completer';
+    return texte.replace("e", " ");
 }
 var concatString = function (texte1, texte2) {
-    return 'A completer';
+    return texte1 + texte2;
 }
 var afficherCar5 = function (texte) {
-    return 'A completer';
+    return texte.charAt(4,5);
 }
 var afficher9Car = function (texte) {
-    return 'A completer';
+    return texte.substring(0,9);
 }
 var majusculeString = function (texte) {
-    return 'A completer';
+    return texte.toUpperCase();
 }
 var minusculeString = function (texte) {
-    return 'A completer';
+    return texte.toLowerCase();
 }
 var SupprEspaceString = function (texte) {
-    return 'A completer';
+    return texte.trim();
 }
 var IsString = function (texte) {
-    return 'A completer';
+    return texte.IsString = true;
 }
+
 var AfficherExtensionString = function (texte) {
-    return 'A completer';
+    return texte.split('.').pop();
 }
 var NombreEspaceString = function (texte) {
-    return 'A completer';
+    return texte.split(' ').length - 1;;
 }
 var InverseString = function (texte) {
-    return 'A completer';
+    return texte.split('').reverse().join('');
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-    return 'A completer';
+    return Math.pow(x, y);
 }
 var valeurAbsolue = function (nombre) {
-    return 'A completer';
+    return Math.abs(nombre);
 }
 var valeurAbsolueArray = function (array) {
-    return 'A completer';
+    return array.map(Math.abs);
 }
 var sufaceCercle = function (rayon) {
-    return 'A completer';
+    return Math.round(Math.PI*rayon*rayon);
 }
 var hypothenuse = function (ab, ac) {
-    return 'A completer';
+    return Math.hypot(ab, ac);
 }
 var calculIMC = function (poids, taille) {
-    return 'A completer';
+    return parseFloat((poids/(taille*taille)).toFixed(2));
 }
